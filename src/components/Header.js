67f7e8d154aeb4ed73ad69ec2632425f294f7056
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-09-13 12:13:43 +0200

    simple

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="header">
       <h1>Pizza Di Napoli</h1>
-      <p>Restaurant & Pizzeria</p>
+      <p>Restaurant & Pizzeria 2</p>
     </header>
   );
 }
