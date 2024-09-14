bc9a186282f3b9d080ae8e90f4e8c075dc89cce7
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-09-14 15:51:33 +0200

    SAFOR-4 : Make the menu clickable

--- src/components/Dish/Dish.js
+++ src/components/Dish/Dish.js
@@ -1,30 +1,7 @@
 import "./Dish.css";
+import createRipple from "../../utils/Ripple";
 
 function Dish(props) {
-  const createRipple = (event) => {
-    const button = event.currentTarget;
-
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-
-    // Use getBoundingClientRect() for more accurate positioning on mobile
-    const buttonRect = button.getBoundingClientRect();
-    const left = event.clientX - buttonRect.left - radius;
-    const top = event.clientY - buttonRect.top - radius;
-
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.left = `${left}px`;
-    circle.style.top = `${top}px`;
-    circle.classList.add("ripple");
-
-    const existingRipple = button.querySelector(".ripple");
-    if (existingRipple) {
-      existingRipple.remove();
-    }
-
-    button.appendChild(circle);
-  };
 
   return (
     <div className="dish-item" onClick={createRipple}>
