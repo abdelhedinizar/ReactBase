8c60c4bd37e811d27d2cbbac9ab0008adcba4364
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-11-22 15:52:37 +0100

    SAFOR-28 : Chatbot

---
+++ src/components/modal/SmallModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SmallModal.css';
+
+function Modal({ show, onClose, children }) {
+  if (!show) {
+    return null; // Don't render anything if `show` is false
+  }
+
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <button className="modal-close" onClick={onClose}>X</button>
+        <div className="modal-body">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
