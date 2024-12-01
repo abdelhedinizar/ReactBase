5fac34b13975abe80633a56dec036dec874f595a
abdelehdinizar <abdelhedinizarcss@gmail.com>
2024-12-01 17:09:13 +0100

    SAFOR-29 : Admin Profil and his App

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,16 @@
-// App.test.js
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-describe('App Component', () => {
-  test('renders Header component', () => {
-    render(<App />);
-    const headerElement = screen.getByRole('banner'); // Assuming Header has a role of 'banner'
-    expect(headerElement).toBeInTheDocument();
-  });
+test('renders Header component', async () => {
+  render(<App />);
+  const headerElement = await screen.findByRole('banner');
+  expect(headerElement).toBeInTheDocument();
+});
 
-  test('renders Menu component', () => {
-    render(<App />);
-    const menuElement = screen.getByText((content, element) => {
-      const hasText = (node) => node.textContent === 'Our Menu';
-      const nodeHasText = hasText(element);
-      return nodeHasText;
-    });
-    expect(menuElement).toBeInTheDocument();
-  });
-
-  test('renders Footer component', () => {
-    render(<App />);
-    const footerElement = screen.getByRole('contentinfo'); // Assuming Footer has a role of 'contentinfo'
-    expect(footerElement).toBeInTheDocument();
-  });
+test('renders Menu component', async () => {
+  render(<App />);
+  const menuElement = await screen.findByText('Loading...');
+  expect(menuElement).toBeInTheDocument();
 });
